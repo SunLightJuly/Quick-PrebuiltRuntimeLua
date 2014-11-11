432429cb921250e63d4b1c692d5a82a6f248a5ab
SunLightJuly <ygqy8181@qq.com>
2014-11-11 19:01:45 +0800

    modified copy_to_quick.sh

--- tools/copy_to_quick.sh
+++ tools/copy_to_quick.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC_DIR=$DIR/../PrebuiltRuntimeLua/runtime
 DIST_DIR=$QUICK_V3_ROOT/quick/templates/lua-template-quick/runtime
 
-rm $SRC_DIR/win32/*.pbd
+rm $SRC_DIR/win32/*.pdb
 rm $SRC_DIR/win32/*.ilk
 sudo find $SRC_DIR/ -name ".DS_Store" -depth -exec rm {} \;
 if [ -d "$DIST_DIR" ]; then
