a330ef7d5cbe41b4a62abcf33cb2638c6f087542
SunLightJuly <ygqy8181@qq.com>
2014-11-17 16:09:51 +0800

    updated (2014.11.17 GMT+8 15:46:16)

--- PrebuiltRuntimeLua/src/framework/init.lua
+++ PrebuiltRuntimeLua/src/framework/init.lua
@@ -22,6 +22,9 @@ THE SOFTWARE.
 
 ]]
 
+--------------------------------
+-- @module init
+
 --[[--
 
 quick framework 初始化
@@ -102,7 +105,7 @@ local CURRENT_MODULE_NAME = ...
 
 cc = cc or {}
 cc.PACKAGE_NAME = string.sub(CURRENT_MODULE_NAME, 1, -6)
-cc.VERSION = "3.3 RC0"
+cc.VERSION = "3.3 final"
 cc.FRAMEWORK_NAME = "quick-cocos2d-x"
 
 if cc.Node.removeTouchEvent then
