204a0f87c3ca5a09eff074d7cfbbe4d0c739b29c
SunLightJuly <ygqy8181@qq.com>
2014-12-23 00:48:35 +0800

    modified creator

--- tools/create_runtime_proj.sh
+++ tools/create_runtime_proj.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$QUICK_V3_ROOT/quick/bin/create_project.sh -p com.quickx.test.runtime -o ~/temp/PrebuiltRuntimeLua
+$QUICK_V3_ROOT/quick/bin/create_project.sh -p com.quickx.test.runtime -o ~/temp/PrebuiltRuntimeLua -lt
